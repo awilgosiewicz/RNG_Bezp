4db1d8225a46edd769beb6b3251576720e55f712
Adrian Wilgosiewicz <wilgosiewicz.adrian@gmail.com>
2021-03-17 19:47:44 +0100

    nowy plik z dzwiekiem, jest 8bit

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#import numpy as np
-#import scipy as sp
-#import matplotlib.pyplot as plt
-#import math
-#import audioop
-#import Pydub
-#import binascii
-#import pyaudio
+import numpy as np
+import scipy
+import scipy as sp
+import math
+import audioop
+import pydub
+import binascii
 
 # S - latest sample - variable name - cur_s
 # SP - last sample - prev_s
@@ -15,7 +14,17 @@
 # n__th - correspondent sample
 # same for SP_n and SPP_n
 
+from matplotlib import pyplot as plt
+from scipy.io import wavfile
+
 wilczur = 10
 if wilczur == 10:
     print("poka wilczura joł")
+
+
+f_name= "white_noise_final2.wav"
+rate, data = sp.io.wavfile.read(f_name)
+
+for i in data:
+    print(data)
 
