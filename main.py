6df472aaa69f1ed455af57f61394a186a1e0410c
Adrian Wilgosiewicz <wilgosiewicz.adrian@gmail.com>
2021-03-17 18:54:32 +0100

    v1

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-#import numpy as np
-#import scipy as sp
-#import matplotlib.pyplot as plt
-#import math
-#import audioop
-#import Pydub
-#import binascii
-#import pyaudio
+import numpy as np
+import scipy as sp
+import math
+import audioop
+import pydub
+import binascii
 
 # S - latest sample - variable name - cur_s
 # SP - last sample - prev_s
@@ -15,7 +13,16 @@
 # n__th - correspondent sample
 # same for SP_n and SPP_n
 
+from matplotlib import pyplot as plt
+from scipy.io import wavfile
+
 wilczur = 10
 if wilczur == 10:
     print("poka wilczura joł")
 
+
+f_name= "white_noise_final.wav"
+fs, wav = wavfile.read(f_name)
+
+print(fs)
+plt.plot(wav)
