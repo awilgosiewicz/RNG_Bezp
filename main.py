214de0511f02c60f908bc3d202c5dc4ee046b7ae
Adrian Wilgosiewicz <wilgosiewicz.adrian@gmail.com>
2021-03-28 16:39:01 +0200

    random_bit problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
 from collections import Counter
-
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import wavfile
@@ -8,7 +9,7 @@ number_of_samples = 100000
 f_name = "white_noise.wav"
 rate, data = wavfile.read(f_name)
 bins = 255
-print(len(data))
+print('data lenght:', len(data))
 tab = []
 c = 0
 for x in data:
@@ -18,18 +19,21 @@ for x in data:
             x = x >> 7
             tab.append(x)
             c += 1
-print(rate)
+print('f[Hz]:', rate)
 
 plt.hist([tab], bins, range=[0, 255])
 plt.show()
-print(max(tab))
-print(min(tab))
+print('Max probe:', max(tab))
+print('Min probe:', min(tab))
 count = Counter(tab)
 prob = []
 for x in count:
     prob.append(count.get(x) / number_of_samples)
 
 entropy = 0
+for x in prob:
+    entropy += x * math.log2(1 / x)
+print('entropy:', entropy)
 
 #tutaj dzialalem ja i nie dziala pozdro
 threshold = 100
@@ -39,15 +43,32 @@ temp_prev = [0]
 curr_samp = []
 runcnt = 0
 
-var = np.var(data[:initial_samples])
+var = np.var(tab[:initial_samples])
 halfvar = var / 2
-print(halfvar)
+print('halfvar:', halfvar)
 
-for i in data[watchdog:threshold]:
-    for x in temp_prev:
-        x = x << 2
-        curr_samp.append(10 + (int.from_bytes(os.urandom(1), byteorder='big') * i + x) % 25)
-        if ((curr_samp - temp_prev) ** 2) < halfvar:
-            curr_samp.append(10 + temp_prev + ((curr_samp ^ watchdog)) % 25)
-print(curr_samp)
+
+
+
+
+
+#for i in tab[watchdog:threshold]:
+#    for x in temp_prev:
+#        x = x << 2
+#        curr_samp.append(10 + (int.from_bytes(os.urandom(1), byteorder='big') * i + x) % 25)
+#        if ((curr_samp - temp_prev) ** 2) < halfvar:
+#            curr_samp.append(10 + temp_prev + ((curr_samp ** watchdog)) % 25)
+#print(curr_samp)
+
+
+if watchdog < threshold:
+    for i, curr_samp in enumerate(tab[watchdog:threshold]):
+        if i>0:
+            curr_samp.append(10 + (int.from_bytes(os.urandom(1), byteorder='big') * i + x) % 25)
+            x = curr_samp[i] << 2
+            while (curr_samp[i]-curr_samp[i-1])**2 < halfvar:
+                curr_samp.append(10+(curr_samp[i-1]+((curr_samp ** watchdog)+runcnt)) % 25)
+                watchdog += 1
+            else:
+
 
