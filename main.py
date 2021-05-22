9940e784efe9af91aad7e475007417396a4b3ffa
Adrian Wilgosiewicz <wilgosiewicz.adrian@gmail.com>
2021-05-22 15:46:58 +0200

    wutututu

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from collections import Counter
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 from scipy.io import wavfile
+import random
 
 
 def sampleGet():
@@ -15,10 +17,10 @@ def sampleGet():
                 x = x >> 7
                 tab.append(x)
                 c += 1
-    return tab
+    return np.uint8(tab)
 
 
-def fancyxor(x1, x2, x3):
+def fancyXor(x1, x2, x3):
     result = 0
     for el in x1:
         result = result ^ el
@@ -30,7 +32,7 @@ def fancyxor(x1, x2, x3):
 
 
 def count_entropy(x, amount_of_samples):
-    #entropy
+    # entropy
     count = Counter(x)
     prob = []
     for x in count:
@@ -41,14 +43,27 @@ def count_entropy(x, amount_of_samples):
     print('entropy: ', entropy)
 
 
+def chunk_data(amount_of_samples, data_for_chunking):
+    amount_of_chunks = amount_of_samples / 12500
+    chunked_array = np.array_split(data_for_chunking, amount_of_chunks)
+    print("Number of chunks:", amount_of_chunks)
+    return chunked_array
+
+
+def halfvar_get(halfvar_data):
+    initial_samples = 1000
+    halfvar_count = stats.iqr(halfvar_data[:initial_samples])
+    return halfvar_count
+
 
 number_of_samples = 100000
-f_name = "hotncoldlpl.wav"
+f_name = "sound_2.wav"
 rate, data = wavfile.read(f_name)
 print('data length:', len(data))
 
-
 samples = sampleGet()
+print(chunk_data(number_of_samples, samples))
+
 count_entropy(samples, number_of_samples)
 plt.hist(samples, bins=256, range=[0, 255], density=True)
 plt.title('Znormalizowany rozkład zmiennych losowych generowanych przez źródło szumu:')
@@ -58,7 +73,6 @@ plt.show()
 
 threshold: int = 100
 watchdog: int = 0
-initial_samples = 1000
 Si = []
 SPi = []
 SPPi = []
@@ -71,38 +85,34 @@ SPP = 0
 random_byte = 0
 random_numbers = []
 
-var = np.var(sampleGet()[:initial_samples])
-halfvar = (var / 2) % 25
-if halfvar >20:
-    halfvar=(halfvar%10)/10
-else:
-    halfvar = (var / 2) % 25
-print('halfvar:', halfvar)
-
+halfvar = halfvar_get(samples)
+print(halfvar)
 
 for i, sample in enumerate(samples):
     SPP = SP
     SP = S
-    S = 10 + (sample * i + (SP << 2)) % 25
+    S = 10 + (sample * i + (SP << 2)) % 256
     watchdog = 0
     while watchdog < threshold:
         if ((S - SP) ** 2) < halfvar:
 
-            S = 10 + (SP + ((S ** watchdog) + runcnt)) % 25
+            S = 10 + (SP + ((S ** watchdog) + runcnt)) % 256
             watchdog += 1
         else:
             Si.append(S)
             watchdog = 100
 
 j = 0
-number_of_random_numbers = 10000
-while j < number_of_random_numbers * 8:
-    random_byte += (1 & fancyxor(Si, SPi, SPPi)) * (2 ** (j % 8))
+number_of_random_numbers = 1000
+while j < number_of_random_numbers:
+    random_byte += (1 & fancyXor(Si, SPi, SPPi)) * (2 ** (j % 8))
     SPi.append(Si[j])
     if j % 8 == 7:
         SPPi.append(Si[j])
-        random_numbers.append(random_byte)
+        random_numbers.append(random_byte % 256)
         random_byte = 0
+    else:
+        random_numbers.append(random.choice(Si))
     j += 1
 
 print(random_numbers)
@@ -113,7 +123,5 @@ plt.xlabel('Wartosc probki (x)')
 plt.ylabel('Czestotliwosc wystepowania (p)')
 plt.show()
 
-DataToSave=np.array(random_numbers)
-output_file = open('file.bin', 'wb')
-DataToSave.tofile(output_file)
-output_file.close()
+DataToSave = np.array(random_numbers)
+np.savetxt('file.txt', DataToSave, delimiter='\n', fmt='%f')
